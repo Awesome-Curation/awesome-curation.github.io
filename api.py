c6dd2705a0860ed229c0393e28fdee10ad897515
Ryan <r.hennings11@gmail.com>
2019-02-05 23:17:09 -0800

    - added token & json header to request - created main()

--- api.py
+++ api.py
@@ -1,26 +1,50 @@
 import requests
 import json
 import re
+import os
 
 awesome = 'awesome-ios'
 user = 'vsouza'
+u2 = 'JohnEstropia' 
+r2 = 'GCDKit'
 api = 'https://api.github.com/'
-debug = False
 
 print("\n\n\n")
 print("$" * 65)
 print("$" * 65)
+def main():
+    #f = open('/tmp/readme2', 'r+')
+    #f.write(text)
+    with open('/Users/ryanhennings/Developer/Python/GitHub-Collections-Filter/sandbox') as f:
+        text = f.read()
+
+    #print(loop_file(text))
+    #print(get_token())
+    get_repo(u2, r2)
+
+def get_token():
+    try:
+        path = os.path.dirname(__file__)
+        with open(path + "/token") as f:
+            return f.readline().strip()
+    except IOError as err:
+        print("Failed to get API token\nCreate a 'token' file in this directory")
 
 def get_repo(user, repo):
-    url  = api + 'repos/' + user + '/' + repo
-    r = requests.get(url)
-    parsed = json.loads(r.text)
-    #print(json.dumps(parsed, indent=4, sort_keys=True))
-    return parsed
+    h = {'Authorization':"token " + get_token(),
+         'Accept':'application/vnd.github.v3+json'}
+    try:
+        url  = api + 'repos/' + user + '/' + repo 
+        r = requests.get(url, headers=h)
+        #print(json.dumps(r.json(), indent=4, sort_keys=True))
+        return r.json()
+    except requests.RequestException as err:
+        print("Unable to get repo data")
 
 def get_readme(user, repo):
+    h = {"Accept":"application/vnd.github.VERSION.raw"}
     url = api + 'repos/' + user + "/" + repo + "/readme"
-    r = requests.get(url, headers={"Accept":"application/vnd.github.VERSION.raw"})
+    r = requests.get(url, headers=h)
     return r.text.encode('utf-8') 
  
  # Extract github link from a string
@@ -42,19 +66,17 @@ def loop_file(text):
             links += get_url(line) + "\n"
     return links[:-1]
 
-#f = open('/tmp/readme2', 'r+')
-#f.write(text)
-with open('/Users/ryanhennings/Developer/Python/GitHub-Collections-Filter/sample') as f:
-    text = f.read()
-
-print(loop_file(text))
-
-
-#print("Full Count: " + str(text.count('\n')))
-#print("Link Count: " + str(links.count('\n')))
-#print("Link Count: " + str(count))
-
+if __name__ == "__main__":
+    main()
 
 print("*" * 65)
 print("*" * 65)
 
+# TODO items
+#forks_count
+#language
+#stargazers_count
+#subscribers_count
+#watchers_count
+# Need to look at status codes for contributors
+#+ /stats/contributors""
