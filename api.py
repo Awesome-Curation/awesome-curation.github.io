d8d007e03410f07fec606abd72ac1bcf6fd1a4f3
Ryan <r.hennings11@gmail.com>
2019-02-07 23:34:45 -0800

    - modified for general api request (can do with readme too if headers are parameter) - added get emojis api

--- api.py
+++ api.py
@@ -2,54 +2,68 @@ import requests
 import json
 import re
 import os
+import emoji
 
 awesome = 'awesome-ios'
 user = 'vsouza'
 u2 = 'JohnEstropia' 
 r2 = 'GCDKit'
-api = 'https://api.github.com/'
+API = 'https://api.github.com/'
+TOKEN = os.environ['TOKEN']
 
 print("\n\n\n")
 print("$" * 65)
 
 def main():
-    #f = open('/tmp/readme2', 'r+')
-    #f.write(text)
+    emojis = get_emojis()
+
     with open('/Users/ryanhennings/Developer/Python/GitHub-Collections-Filter/sandbox') as f:
         text = f.read()
 
-    #for link in get_links(text):
-    #    (user, repo) = get_user_repo(link)
-    #    data = get_repo_data(user, repo)
-    #    console_print(repo, link, data)
+    #rgx = re.compile(':[a-z_]{1,30}:')
+    for x in text.splitlines():
+        if re.search(':[a-z_]{1,30}:', x):
+            print(x)
+        else:
+            print(x)
 
     #link = get_links(text)[0]
     #(user, repo) = get_user_repo(link)
     #data = get_repo_data(user, repo)
     #tr = html_print(repo, link, data)
 
-    footer = "</table></body></html>"
-    path = os.path.dirname(__file__)
-    with open(path + "/table.html", "a") as f:
-        for link in get_links(text):
-            (user, repo) = get_user_repo(link)
-            data = get_repo_data(user, repo)
-            tr = html_print(repo, link, data)
-            f.write(tr)
-        f.write(footer)
+    #footer = "</table></body></html>"
+    #path = os.path.dirname(__file__)
+    #with open(path + "/table.html", "a") as f:
+    #    for link in get_links(text):
+    #        (user, repo) = get_user_repo(link)
+    #        data = get_repo_data(user, repo)
+    #        tr = html_print(repo, link, data)
+    #        f.write(tr)
+    #        break
+    #    f.write(footer)
+
+def formatted_description(text):
+
+    return ""
 
 # Format repo info to an html table row
 def html_print(repo, url, data):
     stars = data['stargazers_count']
+    name = data['name']
     forks = data['forks_count']
     lang = data['language']
+
+    description = formatted_description(data['description'])   
+
     row = ("<tr>\n"
-            "<td> <a href='" + url + "'target='_blank'>" + str(repo) +"</a></td>\n"
-            "<td>"+ str(stars) +"</td>\n"
-            "<td>"+ str(forks) +"</td>\n"
-            "<td>"+ str(lang)  +"</td>\n"
+            "<td> <a href='" + url + "'target='_blank'>" + name +"</a></td>\n"
+            "<td>"+ description +"</td>\n"
+            "<td>"+ str(stars)  +"</td>\n"
+            "<td>"+ str(forks)  +"</td>\n"
+            "<td>"+ str(lang)   +"</td>\n"
             "</tr>\n")
-    return row
+    return row.encode('utf-8')
 
 # Extract username and repo name from github link
 def get_user_repo(link):
@@ -81,13 +95,18 @@ def get_token():
     except IOError as err:
         print("Failed to get API token\nCreate a 'token' file in this directory")
 
-# Get JSON repo data from github api
+def get_emojis():
+    return get_api_data("emojis")
+
 def get_repo_data(user, repo):
-    h = {'Authorization':"token " + get_token(),
+    return get_api_data('repos/' + user + '/' + repo)
+
+# Get JSON repo data from github api
+def get_api_data(url):
+    h = {'Authorization':"token " + TOKEN,
          'Accept':'application/vnd.github.v3+json'}
     try:
-        url  = api + 'repos/' + user + '/' + repo
-        r = requests.get(url, headers=h)
+        r = requests.get(API + url, headers=h)
         # TODO: Handle status_code
         #print(json.dumps(r.json(), indent=4, sort_keys=True))
         return r.json()
@@ -97,7 +116,7 @@ def get_repo_data(user, repo):
 # Get README.md raw data file to string
 def get_readme(user, repo):
     h = {"Accept":"application/vnd.github.VERSION.raw"}
-    url = api + 'repos/' + user + "/" + repo + "/readme"
+    url = API + 'repos/' + user + "/" + repo + "/readme"
     r = requests.get(url, headers=h)
     return r.text.encode('utf-8') 
  
