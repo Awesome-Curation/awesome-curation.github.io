3dd3a855e7a5b666cc0eac28b51f639b8e967fd0
Ryan <r.hennings11@gmail.com>
2019-02-23 16:33:57 -0800

    Add docstring documentation for IDEs

--- src/core.py
+++ src/core.py
@@ -15,13 +15,14 @@ MARKERS = ["Dropdown", "Table"]
 def main():
     start_time = time.time()
 
+    # Testing - contains a sample readme file
     sandbox_readme = get_root_path() + '/data/sandbox_readme'
     with open(sandbox_readme) as f:
         text = f.read()
     
     repos = build_links_dict(text)
-    links = repos['UI']
-    print(api.get_rate_limit('Reset'))
+    links = repos['UI'] # Random sections (contains the most URLs)
+    print(api.get_rate_limit('Remaining'))
 
     destroy_section('Table')
     destroy_section('Dropdown')
@@ -31,13 +32,10 @@ def main():
     elapsed_time = time.time() - start_time
     print('Time elapsed: ' + str(float("%0.4f" % (elapsed_time))))
 
-'''
-MARK: Build Core Data Section
-'''
+##############################################
+# Build Core Data Section
+##############################################
 
-# Create dictionary for links with category
-# Keys - Categories
-# Values - List of repo links
 def build_links_dict(text):
     """ Get all repositories in each category
 
@@ -70,12 +68,11 @@ def build_links_dict(text):
             repos[section] = items    
     return repos
 
-'''
-MARK: HTML Modification Section
-'''
 
-# Delete section from index.html
-# section - marker to look for (ex: Dropdown)
+##############################################
+# HTML Modification Section
+##############################################
+
 def destroy_section(section):
     """ Delete a section of index.html
 
@@ -117,9 +114,6 @@ def destroy_section(section):
         if not end_success:
             print("Unable to find '" + section + "' End marker")
 
-# Insert section into html webpage 
-# date - text to write
-# section - marker to look for (ex: Dropdown)
 def write_section(data, section):
     """ Insert a section of text into an index.html page
 
@@ -129,7 +123,8 @@ def write_section(data, section):
     Acceptable section arguments are explicitly defined in a 
     global list named MARKERS. The string must be in valid HTML
     format.
-        - TODO: Validate acceptable html 
+        - TODO: 
+            * Validate acceptable html 
 
     Args:
         data (str): HTML content to insert
@@ -154,19 +149,24 @@ def write_section(data, section):
             print("Unable to insert '" + section + "' into html page")
 
 
-'''
-MARK: Table Section
-'''
+##############################################
+# Table Section
+##############################################
 
-# Return a string with the html formatted table rows
 def build_table(text):
     """ Setup and HTML formatted table with GitHub repo data
 
     This function will request all data for repositories in a
     given markdown string. The data will be returned in an HTML
     formatted table for all of the links found in the argument.
+    Only markdown bulleted, new line separated strings should
+    be passed to this function. Ex:
+        * [repo_name](https://github.com/user/repo) - Description
     
-    
+    Args:
+        text (str): markdown bullets with GitHub repo links
+    Returns:
+        str: HTML formatted table rows
 
     """
     table = ""
@@ -178,9 +178,21 @@ def build_table(text):
 
     return table
 
-# Get all links from a file/string (\n separated)
-# Return 'list' of links
 def repo_links(text):
+    """ Get a list of repository links
+
+    This function will accept a list or newline separated
+    string of markdown bullets containing GitHub repository
+    links. The GitHub URLs contained in the bullets will be
+    returned as a list of strings.
+
+    Args:
+        text (str, list): markdown bullets containing GitHub URLs
+            - str: newline separated
+            - list[str]: list of bullets
+    Returns:
+        list: GitHub URLs in the argument
+    """
     links=[]
     lines = []
     if isinstance(text, str):
@@ -197,9 +209,22 @@ def repo_links(text):
             
     return links #[:-1]
 
-# Format repo info to an html table row
-# Sometime api returns with values missing. Need to reload if so
 def html_table(repo, url, data):
+    """ Format repository data into an HTML table row
+
+    This function accepts a json dictionary containing the repository
+    data and builds and HTML formatted table row. 
+        - TODO:
+            * Handle bad repo arguments (raise & call a reload)
+            * Handle exceptions (& add one for KeyError 'data' access)
+            * Repo & url arguments are unnecessary & can be removed
+            * Add more table rows (as options to be shown by DataTables lib)
+    
+    Args:
+        repo (str): used to print during exceptions. Should be removed
+        url (str): GitHub URL. Should be available from 'data'
+        data (dict): GitHub json response dictionary
+    """
     stars = data['stargazers_count']
     forks = data['forks_count']
     name = data['name'] if data['name'] != None else "None" # None == NoneType i guess
@@ -227,6 +252,20 @@ def html_table(repo, url, data):
 
 # Format description to include emoji images 
 def format_description(text):
+    """ Format repository description to handle invalid UTF-8 strings
+
+    This function modifies descriptions containing emojis (formatted in 
+    API response as ':emoji_name:'). All descriptions should be passed 
+    here to sanitize the input for a table row. For descriptions containing 
+    emojis, an img html tag with a link to the corresponding GitHub asset
+    will replace the ':emoji_name:' from the original string.
+    
+    Args:
+        text (str): description retrieved from repository
+    Returns:
+        str: formatted string with img emojis if necessary 
+
+    """
     if not text:
         return ''
     
@@ -248,6 +287,19 @@ def format_description(text):
 
 # Read emojis from json text file
 def read_emojis():
+    """ Get emoji dictionary with GitHub assets links
+
+    This function retrieves the emojis from a file and returns
+    the data as a json dictionary. In the event of an error reading
+    the file, another API call will be made to fetch the emojis 
+    again and will return that dictionary. A new file should be
+    created in this case.
+
+    Retruns:
+        dict: emoji names with asset links
+            - {'emoji_name':'https://github.githubassets.com/images/icons/emoji/emoji_name.png?v'}
+
+    """
     g_emojis = get_root_path() + '/data/emojis.txt'
     try:
         with open(g_emojis, 'r') as f:
@@ -262,13 +314,29 @@ def read_emojis():
 
     return json.loads(data)
 
-'''
-MARK: Categories Section
-'''
+
+##############################################
+# Categories Section
+##############################################
 
 # Write categories to html
 # Accepts dictionary (with categories key) or list
 def build_categories(items):
+    """ Build an HTML list of categories
+
+    This function is used to create and HTML formated list of
+    categories to be used in a dropdown menu bar. 
+        - TODO:
+            * Probably not needed. Just returns a function value
+    
+    Args:
+        items (dict, list): categories
+            - dict: key containing categories
+            - list: categories
+    Returns:
+        str: HTML formatted categories
+
+    """
     sections = []
     if isinstance(items, dict):
         for key in items:
@@ -284,6 +352,19 @@ def build_categories(items):
 
 # Format categories to html for dropdown button
 def html_categories(sections):
+    """ Put categories into 'bootstrap-select' formatted dropdown list
+    
+    This function accepts a list of categories and formats them for
+    bootstrap-select to handle in a dropdown menu. Categories are
+    concatinated in the form:
+        <option>Category</option>
+    
+    Args: 
+        sections (list): categories to add in dropdown
+    Returns:
+        str: bootstrap-select formatted categories
+
+    """
     rows = ''
     try:
         for item in sections:
