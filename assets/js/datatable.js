c55fbc72959e1ded9c82bd00c9a1304d0070e53c
Ryan <r.hennings11@gmail.com>
2019-03-09 17:23:13 -0800

    change to ajax json datatable

--- assets/js/datatable.js
+++ assets/js/datatable.js
@@ -1,6 +1,16 @@
 $(document).ready(function () {
     $('#curated').DataTable( {
+        "ajax" : "data/sandbox.json",
+        "columns" : [
+            { "data" : "Repo"},
+            { "data" : "Description"},
+            { "data" : "Stars"},
+            { "data" : "Forks"},
+            { "data" : "Language"}
+        ],
+
         "paging": true,
+        "autoWidth": true,
         "lengthMenu": [ 50, 100, 200, 500 ],
         pageLength: 50,
         "columnDefs": [
@@ -8,7 +18,26 @@ $(document).ready(function () {
             { "orderSequence": [ "asc" , "desc" ], "targets": [ 0, 1, 4 ] },
             // Stars, Forks: Numerical High-Low
             { "orderSequence": [ "desc", "asc" ], "targets": [ 2, 3 ] }
+            //{ width: '20%', 'targets': [0, 2, 3, 4] },
+            //{ width: '60%', 'targets': [1] },
         ],
         responsive: true
+
     });
 })
+
+/*
+function myFunction() {
+    var table = $('#curated').DataTable( {
+    } );
+
+    if (document.getElementById("demo-button").style.color == 'red') {
+        document.getElementById("demo-button").style.color = "white";
+    }
+    else {
+        table.destroy();
+        document.getElementById("demo-button").style.color = "red";
+        //table.reload();
+    }
+}
+*/
