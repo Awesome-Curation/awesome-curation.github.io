bc3788cec36517fa0b3a002a0914a0fc309fb2b7
Ryan <r.hennings11@gmail.com>
2019-03-12 23:38:37 -0700

    add 'All' option to lengthMenu move columns to columnDefs

--- assets/js/datatable.js
+++ assets/js/datatable.js
@@ -3,35 +3,40 @@ $(document).ready(function () {
 
     // Build DataTable instance with custom attributes
     var table = $('#curated').DataTable( {
-        "ajax" : "data/" + awesome_list + "/__all__.json",
-        "columns" : [
-            { "data" : "Repo"},
-            { "data" : "Description"},
-            { "data" : "Stars"},
-            { "data" : "Forks"},
-            { "data" : "Language"}
-        ],
-
-        "paging": true,
-        "autoWidth": true,
-        "lengthMenu": [ 50, 100, 200, 500 ],
-        pageLength: 50,
         "columnDefs": [
+            { "data" : "Repo",        "targets": 0},
+            { "data" : "Description", "targets": 1},
+            { "data" : "Stars",       "targets": 2},
+            { "data" : "Forks",       "targets": 3},
+            { "data" : "Language",    "targets": 4},
+
             // Repo, Description, Language: Alphabetical A-Z
             { "orderSequence": [ "asc" , "desc" ], "targets": [ 0, 1, 4 ] },
-            // Stars, Forks: Numerical High-Low
-            { "orderSequence": [ "desc", "asc" ], "targets": [ 2, 3 ] }
-            //{ width: '20%', 'targets': [0, 2, 3, 4] },
-            //{ width: '60%', 'targets': [1] },
-        ],
-        responsive: true
 
+            // Stars, Forks: Numerical High-Low
+            { "orderSequence": [ "desc", "asc" ], "targets": [ 2, 3 ] },
+
+            // Can't figure out how to not break col 1 w/o ruining sizing (becomes way too big when 'All' showing)
+            { width: '20%', 'targets': [0]},
+            { targets:[0, 1], class:"break"}
+        ],
+
+        "autoWidth": false,
+        "ajax" : "data/" + awesome_list + "/__all__.json",
+        "lengthMenu": [ [50, 100, 200, 500, -1], [50, 100, 200, 500, "All"] ],
+        "paging": true,
+        pageLength: 50,
+        responsive: true
     });
+
+    $('#curated td').css('white-space','initial');
 
     // Load whichever table is selected from the dropdown
     $('.selectpicker').change(function () {
         var selectedItem = $(this).find("option:selected").val();
         file_name = $(this).find("option:selected").attr("id");
-        table.ajax.url( 'data/awesome-ios/' + file_name ).load();
+        table.ajax.url( 'data/' + awesome_list + '/' + file_name ).load();
+        
+        table.columns.adjust().draw();
     });
 });
